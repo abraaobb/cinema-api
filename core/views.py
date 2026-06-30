3210466aa762827257ed7771e809f7a93775fef8
Abraao Brandao <abraao.brandao@fpf.br>
2026-06-30 10:29:26 -0400

    Implement JWT authentication and API permissions
    Integrates `rest_framework_simplejwt` to enable token-based authentication for the API. Sets global DRF default permissions to `IsAuthenticated`, while core viewsets inherit `IsAuthenticatedOrReadOnly` to allow public read access. Exposes endpoints for obtaining and refreshing JWT tokens.

--- core/views.py
+++ core/views.py
@@ -1,23 +1,28 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from core import models, serializers
 
 
-class StudioViewSet(viewsets.ModelViewSet):
+class BaseViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class StudioViewSet(BaseViewSet):
     queryset = models.Studio.objects.all()
     serializer_class = serializers.StudioSerializer
 
 
-class MovieViewSet(viewsets.ModelViewSet):
+class MovieViewSet(BaseViewSet):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
 
 
-class PersonViewSet(viewsets.ModelViewSet):
+class PersonViewSet(BaseViewSet):
     queryset = models.Person.objects.all()
     serializer_class = serializers.PersonSerializer
 
 
-class MoviePersonViewSet(viewsets.ModelViewSet):
+class MoviePersonViewSet(BaseViewSet):
     queryset = models.MoviePerson.objects.all()
     serializer_class = serializers.MoviePersonSerializer
--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from core import views
 
@@ -10,4 +14,9 @@ router.register(r"movie", views.MovieViewSet)
 router.register(r"person", views.PersonViewSet)
 router.register(r"movie-person", views.MoviePersonViewSet)
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router.urls))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
